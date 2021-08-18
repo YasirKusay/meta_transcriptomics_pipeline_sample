723e740bc4eee68a30e63511c064f95212d2f196
YasirKusay <59812220+YasirKusay@users.noreply.github.com>
2021-08-18 15:58:56 +1000

    Update __main__.py

--- meta-transcriptomics_pipeline/__main__.py
+++ meta-transcriptomics_pipeline/__main__.py
@@ -13,13 +13,24 @@ def parse_args():
         default=150,
         help="the length of the fragment sequence"
     )
-
+    
+    parser_detect.set_defaults(func=run_pipeline)
     return parser.parse_args()
 
 
 def main():
+    
+    # centrifuge is not installed locally
+    path_command = subprocess.run('which centrifuge', 
+                                    shell=True, 
+                                    capture_output=True
+                                )
+
+    if (path_command.returncode != 0):
+        raise Exception("centrifuge is not installed or initialised")
+
     args = parse_args()
-    detection(args)
+    args.func(args)
 
 if __name__ == "__main__":
     main()
