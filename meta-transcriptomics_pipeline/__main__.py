3d384e7b546ef9a799637a4d923dae1fce214388
Yasir Kusay <yasirsaad1234@hotmail.com>
2021-09-22 21:26:30 +1000

    file setup and partially implemented QC

--- meta-transcriptomics_pipeline/__main__.py
+++ meta-transcriptomics_pipeline/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import sys 
-from main_pipeline import detection
+import multiprocessing
+from main_pipeline import run_pipeline
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -8,27 +9,70 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--fragment_length",
-        type=int,
-        default=150,
-        help="the length of the fragment sequence"
+        "inp1"
+        type=str,
+        help="read1 input name"
     )
-    
+
+    parser.add_argument(
+        "inp2"
+        type=str,
+        help="read1 input name"
+    )
+
+
+    parser.add_argument(
+        "--qualified_quality_phred"
+        type=int,
+        default=15,
+        help=" DURING QC. The quality value that a base is qualified. Default 15 means phred quality >=Q15 is qualified"
+    )
+
+    parser.add_argument(
+        "--unqualified_percent_limit"
+        type=int,
+        default=45,
+        help=" DURING QC. How many percents of bases are allowed to be unqualified (0~100). Default 40 means 40%"
+    )
+
+    parser.add_argument(
+        "--average_qual"
+        type=int,
+        default=0,
+        help=" DURING QC. if one read's average quality score <avg_qual, then this read/pair is discarded. Default 0 means no requirement (int [=0])"
+    )
+
+    parser.add_argument(
+        "--length_required",
+        type=int,
+        default=15,
+        help="DURING QC. Reads shorter than length_required will be discarded, default is 15. (int [=15])"
+    )
+
+    parser.add_argument(
+        "--complexity_threshold",
+        type=int,
+        default=30,
+        help="DURING QC. The threshold for low complexity filter (0~100). Default is 30, which means 30% complexity is required. (int [=30])"
+    )
+
+    parser.add_argument(
+        "--threads",
+        type=int,
+        default=multiprocessing.cpu_count(),
+        help="the number of cpu threads to use"
+    )
+    # should we have options for frameshift, gapopen, gapextend penalties, and scoring matrix
+    # also an option for tantan repeat masking, composition matrix
+
+    # will we have access to a taxonmap/taxon names for make db
+
+
     parser_detect.set_defaults(func=run_pipeline)
     return parser.parse_args()
 
 
 def main():
-    
-    # centrifuge is not installed locally
-    path_command = subprocess.run('which centrifuge', 
-                                    shell=True, 
-                                    capture_output=True
-                                )
-
-    if (path_command.returncode != 0):
-        raise Exception("centrifuge is not installed or initialised")
-
     args = parse_args()
     args.func(args)
 
--- meta-transcriptomics_pipeline/main_pipeline.py
+++ meta-transcriptomics_pipeline/main_pipeline.py
@@ -1,8 +1,72 @@
 import argparse
 
+def check_fail(command, to_remove):
+    if (command.returncode != 0):
+        exit_now = True
+        print(command.stderr)
+
+    if (exit_now):
+        print("fail")
+        for file in to_remove:
+            os.remove(file)
+        
+    return False    
+
 def run_pipeline(args: argparse.Namespace):
-    results = detect_contamination(args)
-    if (args.graphical_output):
-        draw(results, args.output_file)
-    else:
-        table(results)
+
+    ##################### FASTP ########################
+
+    path_command = subprocess.run('which fastp', 
+                                    check=True,
+                                    shell=True, 
+                                    capture_output=True
+                                )
+
+    fastp_path = path_command.stdout.decode("utf-8").partition('\n')[0]
+
+    if (path_command.returncode != 0):
+        raise Exception("fastp is not installed or initialised")
+
+    fastp_command = fastp_path + 
+                    " --in1 " + args.inp1 + 
+                    " --in2 " + args.inp2 + 
+                    "  --dedup  " + # remove duplicates +
+                    " --qualified_quality_phred  " + args.qualified_quality_phred + 
+                    " --unqualified_percent_limit " + args.unqualified_percent_limit +
+                    " --average_qual " + args.average_qual +
+                    " --length_required " + args.length_required +
+                    # need to consider adapters, should we give the user a chance to add adatpers?
+                    " --thread " + args.threads + 
+           
+    new_command = subprocess.run(fastp_command, shell=True, capture_output=True)
+    if check_fail(new_command, [file1, file2]) is False: return None
+    
+
+
+    #################################### SORTMERNA ############################
+    
+    path_command = subprocess.run('which sortmerna', 
+                                    check=True,
+                                    shell=True, 
+                                    capture_output=True
+                                )
+
+    sortmerna_path = path_command.stdout.decode("utf-8").partition('\n')[0]
+
+    if (path_command.returncode != 0):
+        raise Exception("sortmerna is not installed or initialised")
+
+    sortmerna_command = sortmerna_path + 
+                    " --ref  " + 
+                    " --aligned " + # stores rRNA reads, needs to be deleted afterwards
+                    " --other " + # stores non rRNA reads/ creates 2 files, one for forward and reverse strand
+                    " --fastx " + # output above 2 in fastx
+                    " --reads " + + " --reads " # pe reads, use --reads twice
+                    " -a " + args.threads
+                    # " -e " evalue threshol, default = 1
+                    # below 2 are not necessary to edit, keep them low as they are concerned with rRNA alignment, which we are trying to get rid of anyways
+                    " --num-alignments 1 " + # 1 = all alignments reaching E value threshold are reported, 0 = the first alignment passing E-value threshold
+                    " --best 1 " + # 1 = all high candidate reference sequences will be searched for alignments
+                    
+    new_command = subprocess.run(sortmerna_command, shell=True, capture_output=True)
+    if check_fail(new_command, [file1, file2]) is False: return None
