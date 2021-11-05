83ae92940dc2d7471c25cf94dd5c178a52f4e9e9
Yasir Kusay <yasirsaad1234@hotmail.com>
2021-11-05 13:45:24 +1100

    modified how it operates to go through both the read and protein file

--- meta-transcriptomics_pipeline/merge_sams.py
+++ meta-transcriptomics_pipeline/merge_sams.py
@@ -8,61 +8,40 @@ import subprocess
 from helpers import check_fail
 
 def merge_fasta(snap_sam, diamond_sam, path, seq_type):
-    # lets first merge the 2 files together
-    out = path + "/merged_temp" + seq_type + ".sam" 
-    merge_command = "samtools merge " + out + " " + snap_sam + " " + diamond_sam 
-    new_command = subprocess.run(merge_command, shell=True)
-    if check_fail("samtools merge", new_command, []) is True: return None
-
-    final_file = path + "/merged_" + seq_type + ".sam" 
-    output = open(final_file, "w")
+    output_snap = open(path + "/nucl_alignments.txt", "w")
+    output_diamond = open(path + "/prot_alignments.txt", "w")
+    best_hits = {}
 
     # now we can go about selecting the best hit
-
-    with open(out, "r") as f:
-        prev_query = "NULL"
-        best_query = "NULL"
-        best_edit_dist = -1
-        best_mapq = -1
-        best_line = "NULL"
-        for line in f:
-            curr = line.split()
-            assert curr[12].split(":")[0] == "NM"
-            if (curr[0][0]!="@"):
-                curr_line = line
-                if (prev_query == curr[0]):
-                    if (int(curr[12].split(":")[2]) > best_edit_dist): # checking if line has higher edit dist
-                        best_query = curr[0]
-                        best_line = line
-                        best_edit_dist = int(curr[12].split(":")[2])  
-                    elif (int(curr[12].split(":")[2]) == best_edit_dist): # checking if line has a higher mapq, if edit dists are equal
-                        if (int(curr[4]) > best_mapq and int(curr[4]) != 255):
-                            best_query = curr[0]
-                            best_line = line
-                            best_edit_dist = int(curr[12].split(":")[2])  
-                            best_mapq = int(curr[4])
-                    
-                    pre_query = curr[0]
-
-                else:
-                    if (best_line != "NULL"):
-                        output.write(line)
+    for out in [snap_sam, diamond_sam]:
+        with open(out, "r") as f:
+            for line in f:
+                curr = line.split()
+                assert curr[12].split(":")[0] == "NM"
+                if (curr[0][0]!="@"):
+                    curr_line = line
+                    if (curr[0] in best_hits):
+                        prev_best = best_hits[curr[0]][1]
+                        prev = prev_best.split()
+                        if (int(curr[12].split(":")[2]) > int(prev[12].split(":")[2])): # checking if line has higher edit dist
+                            best_hits[curr[0]] = [out, line]
+                        elif (int(curr[12].split(":")[2]) == int(prev[12].split(":")[2])): # checking if line has a higher mapq, if edit dists are equal
+                            if (int(curr[4]) > int(prev[4]) and int(curr[4]) != 255):
+                                best_hits[curr[0]] = [out, line]
                         
-                    prev_query = curr[0]
-                    best_query = prev_query
-                    best_edit_dist = int(curr[12].split(":")[2])
-                    best_mapq = int(curr[4])
-                    best_line = line
-                    
+                        pre_query = curr[0]
 
-                # we are in the alignment section
+                    else:   
+                        best_hits[curr[0]] = [out, line]
 
-            elif (curr[0][0]=="@"):
-                output.write(line)
-                # we are in the header section
-
-
-    output.close()
-
-    # returning merged file name
-    return out 
+    
+    for key in best_hits:
+        line = best_hits[key][1]
+        curr = line.split()
+        if best_hits[key][0] == snap_sam:
+            output_snap.write(key + '\t' + curr[2])
+        else: 
+            output_diamond.write(key + '\t' + curr[2])
+    
+    output_snap.close()
+    output_diamond.close()
