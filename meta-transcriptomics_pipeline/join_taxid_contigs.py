7549578690a3a66fa6c84260bef8338e90dcaa6d
Yasir Kusay <yasirsaad1234@hotmail.com>
2021-11-08 22:09:40 +1100

    bug fixes in merge_sams and optimised efficiency in join_taxid_contigs

--- meta-transcriptomics_pipeline/join_taxid_contigs.py
+++ meta-transcriptomics_pipeline/join_taxid_contigs.py
@@ -5,13 +5,44 @@
 import subprocess
 from helpers import check_fail
 
-def join(file, mapping_file, join_file):
-    command = "join -1 2 -2 1 -o \"1.1, 1.2, 2.1\" <(sort -k2 " + file + ") <(sort -k1 " + mapping_file + ") >> " + join_file
+def obtain_relevant_taxids(accession_file, mapping_file, write_file):
+
+    mf = open(accession_file, "r")
+    curr_accession = mf.readline()
+    wf = open(write_file, "w")
+    
+    with open(mapping_file, "r") as cf:
+        for line in cf:
+            curr = line.split()
+            if (curr[0] == curr_accession):
+                wf.write(line)
+                curr_accession = mf.readline()
+
+    mf.close()
+    wf.close()
+            
+
+def join(contig_file, read_file, mapping_file, join_file, path):
+
+    # lets firstly join the contig and read files 
+    combined_file_temp = path + "/combined_file_temp.txt"
+    combined_file_sorted = path + "/combined_file_sorted.txt"
+    subprocess.run("cat " + contig_file + " > " + combined_file_temp, shell=True)
+    subprocess.run("cat " + read_file + " > " + combined_file_temp, shell=True)
+    subprocess.run("sort -k2 " + combined_file_temp + " > " + combined_file_sorted, shell=True)
+
+    unique_accessions = path + "/unique_accessions.txt"
+    subprocess.run("cut -f2 | uniq " + combined_file_sorted + " > " + unique_accessions, shell=True)
+
+    relevant_taxids = path + "/relevant_taxids.txt"
+    obtain_relevant_taxids(unique_accessions, mapping_file, relevant_taxids)
+
+    command = "join -1 2 -2 1 -o \"1.1, 1.2, 2.1\" <(sort -k2 " + combined_file_sorted + ") <(sort -k1 " + relevant_taxids + ") >> " + join_file
     new_command = subprocess.run(command, shell=True)
     if check_fail("join", new_command, []) is True: return None
     
-def join_taxid_contigs(output_snap, output_diamond, nucl_map, prot_map, final_out):
-    if join(output_snap, nucl_map, final_out) == None or join(output_diamond, prot_map, final_out) == None: 
+def join_taxid_contigs(output_contig_snap, output_read_snap, output_contig_diamond, output_read_diamond, nucl_map, prot_map, final_out, path):
+    if join(output_contig_snap, output_read_snap, nucl_map, final_out, path) == None or join(output_contig_diamond, output_read_diamond, prot_map, final_out, path) == None: 
         return False
 
     return True
--- meta-transcriptomics_pipeline/merge_sams.py
+++ meta-transcriptomics_pipeline/merge_sams.py
@@ -3,7 +3,6 @@
 # best hits are decided by edit distance (1) and mapq (2)
 
 import subprocess
-from helpers import check_fail
 
 def merge_sams(snap_sam, diamond_sam, path):
     output_snap = open(path + "/nucl_alignments.txt", "w")
@@ -11,56 +10,54 @@ def merge_sams(snap_sam, diamond_sam, path):
 
     # need to firstly merge and sort the 2 files
     snap_diamond_combined_file = path + "/snap_diamond_combined_file"
-    command_1 = 'egrep -v "^@" ' + output_snap + " > " + snap_diamond_combined_file
+    command_1 = 'egrep -v "^@" ' + snap_sam + " > " + snap_diamond_combined_file
     command = subprocess.run(command_1, shell=True) 
-    command_2 = 'egrep -v "^@" ' + output_diamond + " >> " + snap_diamond_combined_file
+    command_2 = 'egrep -v "^@" ' + diamond_sam + " >> " + snap_diamond_combined_file
     command = subprocess.run(command_2, shell=True) 
 
     # now lets sort based on read/contig id
     snap_diamond_sorted_file = path + "/snap_diamond_sorted_file"
-    sort_command = "sort -k1 " + snap_diamond_combined_file + snap_diamond_sorted_file
+    sort_command = "sort -k1 " + snap_diamond_combined_file + " > " + snap_diamond_sorted_file
     command = subprocess.run(sort_command, shell=True) 
 
     # now we can go about selecting the best hit
     with open(snap_diamond_sorted_file, "r") as f:
         prev_query = "NULL"
-        best_query = "NULL"
         best_edit_dist = -1
         best_mapq = -1
         best_line = "NULL"
         for line in f:
             curr = line.split()
             assert curr[12].split(":")[0] == "NM"
-            if (curr[0][0]!="@"):
-                curr_line = line
-                if (prev_query == curr[0]):
-                    if (int(curr[12].split(":")[2]) > best_edit_dist): # checking if line has higher edit dist
-                        best_query = curr[0]
+            curr_line = line
+            if (prev_query == curr[0]):
+                if (int(curr[12].split(":")[2]) > best_edit_dist): # checking if line has higher edit dist
+                    best_line = line
+                    best_edit_dist = int(curr[12].split(":")[2])  
+                    best_mapq = int(curr[4])
+                elif (int(curr[12].split(":")[2]) == best_edit_dist): # checking if line has a higher mapq, if edit dists are equal
+                    if (int(curr[4]) > best_mapq and int(curr[4]) != 255):
                         best_line = line
                         best_edit_dist = int(curr[12].split(":")[2])  
-                    elif (int(curr[12].split(":")[2]) == best_edit_dist): # checking if line has a higher mapq, if edit dists are equal
-                        if (int(curr[4]) > best_mapq and int(curr[4]) != 255):
-                            best_query = curr[0]
-                            best_line = line
-                            best_edit_dist = int(curr[12].split(":")[2])  
-                            best_mapq = int(curr[4])
-                    prev_query = curr[0]
+                        best_mapq = int(curr[4])
+                prev_query = curr[0]
 
-                else:
-                    if (best_line != "NULL"):
-                        # diamond file, because it stores E-value 
-                        if curr[15].split(":")[0] == "ZE":
-                            output_diamond.write(curr_line)
-                        else:
-                            output_snap.write(curr_line)
+            else:
+                if (best_line != "NULL"):
+                    # diamond file, because it stores E-value 
+                    to_print = best_line.split()
+                    if to_print[15].split(":")[0] == "ZE":
+                        output_diamond.write(to_print[0] + "\t" + to_print[2])
+                    else:
+                        output_snap.write(to_print[0] + "\t" + to_print[2])
 
-                    prev_query = curr[0]
-                    best_query = prev_query
-                    best_edit_dist = int(curr[12].split(":")[2])
-                    best_mapq = int(curr[4])
-                    best_line = line
+                prev_query = curr[0]
+                best_edit_dist = int(curr[12].split(":")[2])
+                best_mapq = int(curr[4])
+                best_line = line
 
     command = subprocess.run("rm " + snap_diamond_combined_file, shell=True)
+    command = subprocess.run("rm " + snap_diamond_sorted_file, shell=True)
     
     output_snap.close()
     output_diamond.close()
