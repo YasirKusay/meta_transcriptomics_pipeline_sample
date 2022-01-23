f19356e269afeccfe220783052f0a89c11226326
Yasir Kusay <yasirsaad1234@hotmail.com>
2022-01-24 05:10:55 +1100

    implemented retrieve lineage info

---
+++ meta_transcriptomics_pipeline/get_lineage_info.py
@@ -0,0 +1,31 @@
+from ete3 import NCBITaxa
+
+def get_taxids(input_file):
+    taxids = {}
+    with open(input_file, "r") as f:
+        for line in f:
+            curr = line.split()
+            taxid = curr[0]
+            abundance = curr[1]
+            taxids[taxid] = abundance
+
+    return taxids
+
+def get_lineage_info(input_file, output_file):
+    taxids = get_taxids(input_file)
+    ranks = ['superkingdom', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
+    wf = open(output_file, "w")
+    ncbi = NCBITaxa()
+
+    for taxid in taxids.keys():
+        lineage = ncbi.get_lineage(taxid)
+        lineage2ranks = ncbi.get_rank(lineage)
+        ranks2lineage = dict((rank, taxid) for (taxid, rank) in lineage2ranks.items())
+        wf.write(taxid)
+        for rank in ranks:
+            r = ranks2lineage.get(rank, 'Unknown')
+            wf.write("\t" + r)
+            
+        wf.write("\n")
+
+    wf.close()
