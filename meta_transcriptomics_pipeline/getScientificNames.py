305a54715e07adb8e4d8d88ba3b3e8b034515cdd
Yasir <z5215055@ad.unsw.edu.au>
2022-01-25 12:25:15 +1100

    unassigned taxids get their own taxids as their scientific name

--- meta_transcriptomics_pipeline/getScientificNames.py
+++ meta_transcriptomics_pipeline/getScientificNames.py
@@ -20,13 +20,24 @@ def getScientificNames(taxids, folder_location):
                     if inc == len(taxids):
                         break
             elif int(curr[0]) > int(taxids[inc]): # has been a problem before, e.g. #115782 is not in names.dmp for some reason, simply skips that taxid
+                sci_names[curr[0]] = taxids[inc] 
+                # if taxid is not in names.dmp, simply have the "scientific name" be its taxid
                 inc += 1
+
                 # good idea to check anyways, in case we may accidentally skip it
+                
                 if int(curr[0]) == int(taxids[inc]):
                     if curr[3] == "scientific name\t|\n":
                         sci_names[curr[0]] = curr[1]
                         inc += 1
                         if inc == len(taxids):
                             break
+
+
+    if (inc < len(taxids)):
+        while (inc < len(taxids)):
+            sci_names[taxids[inc]] = taxids[inc]
+            inc += 1    
+
 
     return sci_names
