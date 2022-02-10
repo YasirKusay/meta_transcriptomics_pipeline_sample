f7c4c74ac6e236d53d3f4a9cd6ba11a513bd1cf9
Yasir Kusay <yasirsaad1234@hotmail.com>
2022-02-11 09:53:03 +1100

    removed unneccessarry files

--- meta_transcriptomics_pipeline/adjust_tpms.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def adjust_tpms(read1, read2, tpm_file):
-    cmd = "cat " +  tpm_file + " | awk 'NR > 1' | sort -n -k 6 | uniq"
-    
--- meta_transcriptomics_pipeline/sample.py
+++
@@ -1,5 +0,0 @@
-with open("samp2.txt", "r") as f:
-    for line in f:
-        l = line.split()
-        print(len(l))
-    
