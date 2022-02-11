9df2cd61c56eea45043e74f667309dbf39780ac8
Yasir <z5215055@ad.unsw.edu.au>
2022-02-11 12:29:02 +1100

    further bug fixes

--- meta_transcriptomics_pipeline/merge_sams.py
+++ meta_transcriptomics_pipeline/merge_sams.py
@@ -40,10 +40,11 @@ def merge_sams(snap_sam, diamond_sam, path, snap_out = None, diamond_out = None)
         best_line = "NULL"
         for line in f:
             curr = line.split("\t")
+            print(curr)
             if (curr[2] != "*"):
                 edit_dist_inc = 11
                 while (edit_dist_inc < len(curr)):
-                    if curr[best_edit_dist].split(":")[0] == "NM":
+                    if curr[edit_dist_inc].split(":")[0] == "NM":
                         break
                     edit_dist_inc += 1
 
