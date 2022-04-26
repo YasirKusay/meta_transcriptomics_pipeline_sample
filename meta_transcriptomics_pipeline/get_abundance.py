a4ce69eb4d4b70cd3efe1c2dc5efde2d6b5158c0
Yasir <z5215055@ad.unsw.edu.au>
2022-04-26 20:07:59 +1000

    n50 option added

---
+++ meta_transcriptomics_pipeline/get_abundance.py
@@ -0,0 +1,74 @@
+def get_abundance(joined, total_reads, n50, final_file, contaminants):
+    final_res = {}
+    taxids = {}
+    lengths = {}
+    counts = {}
+
+    denom = 0.0
+
+    with open(joined, "r") as f:
+        for line in f:
+            curr = line.split()
+            if (curr[3] in contaminants):
+                total_reads = total_reads - int(curr[2])
+
+    denom = total_reads
+
+    with open(joined, "r") as f:
+        for line in f:
+            curr = line.split()
+            if (int(curr[1]) < 50):
+                continue
+            if (curr[3] in contaminants): # skipping contaminants
+                continue
+            name = curr[0]
+            length = int(curr[1])
+            if (!name.startswith("k")): # is not a contig
+                length = n50
+            count = int(curr[2])
+            taxid = int(curr[3])
+
+            taxids[name] = taxid
+            lengths[name] = length
+            counts[name] = count
+
+    # solution below adapted from RSEM
+    # https://github.com/deweylab/RSEM/blob/e4dda70e90fb5eb9b831306f1c381f8bbf71ef0e/WriteResults.h
+    # function adapted: calcExpressionValues
+    # only difference is I did not use expected length, using the real length instead
+
+    for key in lengths:
+        counts[key] = counts[key]/denom
+
+    fpkm = {}
+    # calculate FPKM
+    for key in lengths:
+        fpkm[key] = (counts[key] * 1e9)/lengths[key]
+
+    denom = 0.0
+    for key in lengths:
+        denom += fpkm[key]
+
+    tpm = {}
+    for key in lengths:
+        tpm[key] = fpkm[key]/(denom * 1e6)
+
+    for key in tpm:
+        print(str(key) + "\t" + str(tpm[key]) + "\n")
+
+    final_tpm = {}
+    # calculating tpm but for the taxids
+    for key in lengths:
+        if taxids[key] in final_tpm.keys():
+            final_tpm[taxids[key]] += tpm[key]
+        else:
+            final_tpm[taxids[key]] = tpm[key]
+
+    if os.path.isfile(final_file):
+        os.remove(final_file)
+    wf = open(final_file, "w")
+    final_tpm_sorted = dict( sorted(final_tpm.items(), key=operator.itemgetter(1),reverse=True))
+    for key in final_tpm_sorted:
+        if (key not in contaminants):
+            wf.write(str(key) + "\t" + str(final_tpm_sorted[key]) + "\n")
+    wf.close()
