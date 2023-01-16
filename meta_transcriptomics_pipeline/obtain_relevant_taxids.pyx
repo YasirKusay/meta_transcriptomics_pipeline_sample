591de7946e461b08b3fc3af5e6893fefc9a6422d
Yasir Kusay <yasirsaad1234@hotmail.com>
2023-01-16 11:23:04 +1100

    forgot to commit this (this function was modified in October 2022), changed such that it compares the accession numbers rather than the accession versions (in case the accession versions fail to match). In addition, I also forgot to push the tests for this and the other functions, will do so soon once I verify everything

--- meta_transcriptomics_pipeline/obtain_relevant_taxids.pyx
+++ meta_transcriptomics_pipeline/obtain_relevant_taxids.pyx
@@ -1,5 +1,5 @@
-from libc.stdio cimport FILE, fopen, getline, printf, fclose, fprintf
-from libc.string cimport strcmp, strlen
+from libc.stdio cimport FILE, fopen, getline, fclose, fprintf
+from libc.string cimport strcmp, strlen, strncpy
 from libc.stdlib cimport malloc, free
 
 cdef extern from "stdio.h":
@@ -8,90 +8,99 @@ cdef extern from "stdio.h":
 def obtain_relevant_taxids(str accession_file, str mapping_file, str write_file):
 
     cdef int size, curr_iter, len_accession, len1, i
-    cdef char *line
+    cdef char *curr_mapping_file_line
     cdef char *curr_accession 
     cdef int cancel
-    cdef char *curr = <char *>malloc(100 * sizeof (char))
-    cdef FILE *mf 
+    cdef char *curr_mapping_file_accession = <char *>malloc(100 * sizeof (char))
+    cdef FILE *af 
     cdef FILE *wf
-    cdef FILE *cf
+    cdef FILE *mf
     cdef size_t big_size, small_size
-    cdef ssize_t big_ssize, small_ssize
+    cdef ssize_t mapping_file_bytes, accession_file_bytes
 
     for n in range(100):
-        curr[n] = '\0'
+        curr_mapping_file_accession[n] = '\0'
 
     size = 0
-    line = NULL
+    curr_mapping_file_line = NULL
 
-    mf = fopen(accession_file.encode(), "r")
+    af = fopen(accession_file.encode(), "r")
+
+    curr_accession = NULL
+    small_size = 0
     while (True):
-        small_ssize = getline(&curr_accession, &small_size, mf)
-        if (small_ssize < 0): 
+        accession_file_bytes = getline(&curr_accession, &small_size, af)
+        if (accession_file_bytes < 0): 
             break
         size += 1
 
-    fclose(mf)
-    
-    mf = fopen(accession_file.encode(), "r")
-    small_ssize = getline(&curr_accession, &small_size, mf)
+    fclose(af)
+
+    af = fopen(accession_file.encode(), "r")
+    accession_file_bytes = getline(&curr_accession, &small_size, af)
     len_accession = strlen(curr_accession)
     if (curr_accession[len_accession - 1] == "\n"):
-        curr_accession[len_accession-1] = '\0';
+        curr_accession[len_accession - 1] = '\0';
+    
     wf = fopen(write_file.encode(), "w")
 
     curr_iter = 0
 
-    cf = fopen(mapping_file.encode(), "r")
-    big_ssize = getline(&line, &big_size, cf)
+    mf = fopen(mapping_file.encode(), "r")
+    curr_mapping_file_line = NULL
+    big_size = 0
+    mapping_file_bytes = getline(&curr_mapping_file_line, &big_size, mf)
+
     while True:
-        big_ssize = getline(&line, &big_size, cf)
-        if big_ssize < 0:
+        mapping_file_bytes = getline(&curr_mapping_file_line, &big_size, mf)
+        if mapping_file_bytes < 0:
             break
-        len1 = strlen(line)
+        len1 = strlen(curr_mapping_file_line)
         for i in range(0, len1 - 1):
-            if line[i] == '\0' or line[i] == '\t' or line[i] == '\n':
-                curr[i] = '\0'
+            if curr_mapping_file_line[i] == '\0' or curr_mapping_file_line[i] == '\n':
                 break
-            curr[i] = line[i];
-        if (strcmp(curr, curr_accession) == 0):
-            fprintf(wf, line)
+            if curr_mapping_file_line[i] == '\t':
+                strncpy(curr_mapping_file_accession, curr_mapping_file_line, i);
+                curr_mapping_file_accession[i] = "\0"
+                break
+        if (strcmp(curr_mapping_file_accession, curr_accession) == 0):
+            fprintf(wf, curr_mapping_file_line)
             curr_iter += 1
             if (curr_iter == size):
                 break
-            small_ssize = getline(&curr_accession, &small_size, mf)
+            accession_file_bytes = getline(&curr_accession, &small_size, af)
             len_accession = strlen(curr_accession)
             if (curr_accession[len_accession - 1] == "\n"):
-                curr_accession[len_accession-1] = '\0';
+                curr_accession[len_accession - 1] = '\0';
            
-        elif (strcmp(curr, curr_accession) > 0):
+        elif (strcmp(curr_mapping_file_accession, curr_accession) > 0):
             cancel = 0
-            while (strcmp(curr, curr_accession) > 0):
+            while (strcmp(curr_mapping_file_accession, curr_accession) > 0):
                 curr_iter += 1
                 if (curr_iter == size):
                     cancel = 1
                     break
-                small_ssize = getline(&curr_accession, &small_size, mf)
+                accession_file_bytes = getline(&curr_accession, &small_size, af)
                 len_accession = strlen(curr_accession)
                 if (curr_accession[len_accession - 1] == "\n"):
-                    curr_accession[len_accession-1] = '\0';
-                if (strcmp(curr, curr_accession) == 0):
+                    curr_accession[len_accession - 1] = '\0';
+                if (strcmp(curr_mapping_file_accession, curr_accession) == 0):
                     break
 
             if cancel == 1:
                 break
 
-            if (strcmp(curr, curr_accession) == 0):
-                fprintf(wf, line)
+            if (strcmp(curr_mapping_file_accession, curr_accession) == 0):
+                fprintf(wf, curr_mapping_file_line)
                 curr_iter += 1
                 if (curr_iter == size):
                     break
-                small_ssize = getline(&curr_accession, &small_size, mf)
+                accession_file_bytes = getline(&curr_accession, &small_size, af)
                 len_accession = strlen(curr_accession)
                 if (curr_accession[len_accession - 1] == "\n"):
                     curr_accession[len_accession - 1] = '\0';
 
-    fclose(mf)
+    fclose(af)
     fclose(wf)
-    fclose(cf)
+    fclose(mf)
 
