91900d298b767e89ebb939cb08da5401452a7034
Yasir Kusay <yasirsaad1234@hotmail.com>
2023-01-16 12:00:08 +1100

    changed it such that the program can use the original mapping file 'as is', without any need to modify it manually

--- meta_transcriptomics_pipeline/join_taxid_contigs.py
+++ meta_transcriptomics_pipeline/join_taxid_contigs.py
@@ -13,7 +13,7 @@ def join_accessions_taxids(combined_file_sorted, relevant_taxids, out):
         for line in f:
             curr = line.split()
             accession = curr[0]
-            taxid = curr[1]
+            taxid = curr[2]
             accessions_mapped[accession] = taxid
 
     wf = open(out, "a")
@@ -21,44 +21,48 @@ def join_accessions_taxids(combined_file_sorted, relevant_taxids, out):
         for line in f:
             curr = line.split()            
             read = curr[0]
-            accession = curr[1]
+            full_accession = curr[1]
+            accession = full_accession.split(".")[0]
+            print(accession)
             if (accession in accessions_mapped.keys()):
                 wf.write(read + "\t" + accession + "\t" + accessions_mapped[accession] + "\n")
 
     wf.close()
 
-def join(contig_file, read_file, mapping_file, join_file, path):
+def join(accession_file, mapping_file, join_file, path):
+
+    if (path != ""):
+        path = path + "/"
 
     # lets firstly join the contig and read files 
-    combined_file_temp = path + "/combined_file_temp.txt"
-    combined_file_sorted = path + "/combined_file_sorted.txt"
-    new_command = subprocess.run("cat " + contig_file + " | cut -f1,2" + " > " + combined_file_temp, shell=True)
-    new_command = subprocess.run("cat " + read_file + " | cut -f1,2" + " >> " + combined_file_temp, shell=True)
+    combined_file_temp = path + "combined_file_temp.txt"
+    combined_file_sorted = path + "combined_file_sorted.txt"
+    new_command = subprocess.run("cat " + accession_file + " | cut -f1,2" + " > " + combined_file_temp, shell=True)
     new_command = subprocess.run("LC_COLLATE=C sort -k2 " + combined_file_temp + " > " + combined_file_sorted, shell=True)
 
-    unique_accessions = path + "/unique_accessions.txt"
-    new_command = subprocess.run("cut -f2 " + combined_file_sorted + " | uniq | egrep -v 'SNAP|Illumina|unsorted|LN:' | LC_COLLATE=C sort -k1  > " + unique_accessions, shell=True)
+    unique_accessions = path + "unique_accessions.txt"
+    new_command = subprocess.run("cut -f2 " + combined_file_sorted + " | uniq | egrep -v 'SNAP|Illumina|unsorted|LN:' | LC_COLLATE=C sort -k1 | cut -d'.' -f1 > " + unique_accessions, shell=True)
     # for the above command, some of the nt accessions may have e.g. NR_001_name, hence cut -d'_' -f1,2 takes care of that
 
-    combined_file_sorted_sorted = path + "/combined_file_sorted_sorted"
+    combined_file_sorted_sorted = path + "combined_file_sorted_sorted"
     new_command = subprocess.run("LC_COLLATE=C sort -k2 " + combined_file_sorted + " > " + combined_file_sorted_sorted, shell = True)
     
-    relevant_taxids = path + "/relevant_taxids.txt"
+    relevant_taxids = path + "relevant_taxids.txt"
+    #for file in mapping_file:
     for file in mapping_file:
         obtain_relevant_taxids(unique_accessions, file, relevant_taxids)
-        relevant_taxids_sorted = path + "/relevant_taxids_sorted"
+        relevant_taxids_sorted = path + "relevant_taxids_sorted"
         new_command = subprocess.run("LC_COLLATE=C sort -k1 " + relevant_taxids + " > " + relevant_taxids_sorted, shell = True)
         join_accessions_taxids(combined_file_sorted_sorted, relevant_taxids_sorted, join_file)
         os.remove(relevant_taxids)
 
     return True
     
-def join_taxid_contigs(output_contig_snap, output_read_snap, output_contig_diamond, output_read_diamond, nucl_map, prot_map, final_out, path):
+def join_taxid_contigs(output_contig_snap, output_contig_diamond, nucl_map, prot_map, final_out, path):
     relevant_taxids = path + "/relevant_taxids.txt"
     if os.path.isfile(relevant_taxids):
         os.remove(relevant_taxids)
-    if join(output_contig_snap, output_read_snap, nucl_map, final_out, path) is False or join(output_contig_diamond, output_read_diamond, prot_map, final_out, path) is False: 
+    if join(output_contig_snap, nucl_map, final_out, path) is False or join(output_contig_diamond, prot_map, final_out, path) is False: 
         return False
 
     return True
-    
