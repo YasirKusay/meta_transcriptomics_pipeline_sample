cf5b37acb2c11abc581c5518379993dd27e4ba9e
YasirKusay <yasirsaad1234@hotmail.com>
2023-01-17 12:13:41 +1100

    finished adding tests for obtain_relevant_taxids

---
+++ tests/samples/join/generate_mapping_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "accession\taccession.version\ttaxid\tgi" > mapping.txt
+echo -e "a1\ta1.1\tt1\tg1" >> mapping.txt
+echo -e "a2\ta2.1\tt5\tg5" >> mapping.txt
+echo -e "a3\ta3.2\tt7\tg7" >> mapping.txt
+echo -e "a4\ta4.2\tt1\tg1" >> mapping.txt
+echo -e "a6\ta6.2\tt1\tg1" >> mapping.txt
+echo -e "a7\ta7.3\tt1\tg1" >> mapping.txt
+echo -e "a8\ta8.1\tt5\tg5" >> mapping.txt
+echo -e "a91\ta91.4\tt5\tg5" >> mapping.txt
+echo -e "a92\ta92.1\tt7\tg7" >> mapping.txt
+echo -e "a94\ta94.9\tt1\tg1" >> mapping.txt
+echo -e "a95\ta95.1\tt5\tg5" >> mapping.txt
+echo -e "a96\ta96.1\tt7\tg7" >> mapping.txt
+echo -e "a97\ta97.13\tt2\tg2" >> mapping.txt
+echo -e "a98\ta98.1\tt1\tg1" >> mapping.txt
+echo -e "a99\ta99.6\tt3\tg3" >> mapping.txt
