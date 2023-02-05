9ef4793d7c5249cf3f4c83d396b7acfa4d19eed0
YasirKusay <yasirsaad1234@hotmail.com>
2023-02-05 14:27:19 +1100

    fixed version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy as np
 
 setup(
     name="meta_transcriptomics_pipeline",
-    version="0.2.0",
+    version="0.2.1",
     license='MIT',
     packages=find_packages(include=['meta_transcriptomics_pipeline']),
     author="Yasir Kusay",
