84b715f5d9b140d24ed02065b406c4af1bd3943d
LilWinx <lil.winx@gmail.com>
2024-01-23 11:13:42 +1100

    add staxids&sscinames to outfmt

--- meta_transcriptomics_pipeline/blast_sr.py
+++ meta_transcriptomics_pipeline/blast_sr.py
@@ -17,7 +17,7 @@ def blast_sr(args: argparse.Namespace):
     combined_file_fa = dirpath + "/preprocessing/combined_sr_file.fa"
     
     start = time.time()
-    blast_command = blast_path + " -query " + combined_file_fa + " -db " + args.blast_ncbi_nt_database + " -out " + nt_alignments_file + " -outfmt \"6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen\" -max_target_seqs 10 -num_threads " + str(args.threads)
+    blast_command = blast_path + " -query " + combined_file_fa + " -db " + args.blast_ncbi_nt_database + " -out " + nt_alignments_file + " -outfmt \"6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen staxids sscinames\" -max_target_seqs 10 -num_threads " + str(args.threads)
     run_shell_command(blast_command)
 
     end = time.time()
